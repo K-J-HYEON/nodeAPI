6d6139b7c80d57216ecbe9ff792b2062af3bf64e
K-J-HYEON <kjh950601@naver.com>
2022-01-02 14:40:33 +0900

    nodeJS

--- Pug/app.js
+++ Pug/app.js
@@ -11,6 +11,8 @@ const userRouter = require('./routes/user');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -30,7 +32,6 @@ app.use(session({
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
-
 
 app.use((req, res, next) => {
   res.status(404).send('Not Found');
