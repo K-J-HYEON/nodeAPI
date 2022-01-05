5cef4eb6e0ea09a4f9712849eb8629ab28745984
K-J-HYEON <kjh950601@naver.com>
2022-01-05 19:20:39 +0900

    nodeJS

--- learn-sequelize/models/user.js
+++ learn-sequelize/models/user.js
@@ -1,5 +1,16 @@
 const Sequelize = require('sequelize');
 
 module.exports = class User extends Sequelize.Model {
-    
+    static init(sequelize) {
+        return super.init({
+            id: {
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING(20),
+                allowNull: false,
+                unique: true,
+            }
+        })
+    }
 }
